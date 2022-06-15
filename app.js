a1c5c8d79d3a320087aa1d3aaf9a3ccbb153d3b5
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-06-15 12:26:16 +0300

    rename

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('mousemove', function (e) {
+  let body = document.querySelector('body');
+  let heart = document.createElement('span');
+  let x = e.offsetX;
+  let y = e.offsetY;
+});
