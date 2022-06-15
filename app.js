ea5710f098d93cd51e4c1814ce4017e523859bbc
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-06-15 12:28:04 +0300

    js

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ document.addEventListener('mousemove', function (e) {
   let heart = document.createElement('span');
   let x = e.offsetX;
   let y = e.offsetY;
+  heart.style.left = x + 'px';
+  heart.style.top = y + 'px';
+  body.appendChild(heart);
 });
